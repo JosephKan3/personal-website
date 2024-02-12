685939b37ed9dbf2b2a65e00ae60c2456604293c
DrainCena <josephkan3@gmail.com>
2024-02-12 00:36:15 -0500

    feat: adds og image meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,6 +60,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Joseph Kan</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="sitePreview.jpg" />
       </Head>
       <div className="container">
         {/* Introduction */}
